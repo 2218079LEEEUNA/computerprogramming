f30c3055293d3f4cf43e21c2be50001d4daf644b
2218079LEEEUNA <144421698+2218079LEEEUNA@users.noreply.github.com>
2023-09-08 16:39:01 +0900

    Create 퀴즈 1번.py

---
+++ 퀴즈 1번.py
@@ -0,0 +1,4 @@
+name = input("이름을 입력하세요")
+number_one = input("숫자를 입력하세요")
+number_two = input("숫자를 입력하세요")
+print(f"안녕하세요.{name}님. 입력하신 두 숫자는 {number_one},{number_two}이며, 두 숫자의 곱은 다음과 같습니다:{number_one}*{number_two}")
